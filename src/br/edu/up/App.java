b7daebd2aac9fdb19776bdd16082cffbb06ab0a5
Tonygcm <glincon44@gmail.com>
2024-04-08 20:59:10 -0300

    atualizações

--- src/br/edu/up/App.java
+++ src/br/edu/up/App.java
@@ -1,7 +1,12 @@
 package br.edu.up;
 
+import br.edu.up.modelos.Garcom;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Garcom g = new Garcom();
+        g.setNome("João");
+
     }
 }
--- src/br/edu/up/modelos/Pessoa.java
+++ src/br/edu/up/modelos/Pessoa.java
@@ -1,4 +1,14 @@
 package br.edu.up.modelos;
+
 public class Pessoa {
-    
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
